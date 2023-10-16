8575063371a0ae6c0378275a611a263182651f26
Pawan Mehra <61104663+Pawan-CEO@users.noreply.github.com>
2023-10-16 12:28:49 +0000

    update

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import streamlit as st
+
+# Add the following line to link your CSS file in your Streamlit app
+st.markdown('<style>body { background-color: red; }</style>', unsafe_allow_html=True)
+
+
+
 st.write("hi")
+e=st.text_input("pawan m")
+
+st.write(e)
+
+st.snow()
+
+
+
